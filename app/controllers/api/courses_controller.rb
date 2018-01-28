788403fc4702cfb5c75a9f7d4f23c3377f0398a9
Carlos Jaramillo <cejaramillof@gmail.com>
2018-01-28 10:38:28 -0500

    servicio web api

---
+++ app/controllers/api/courses_controller.rb
@@ -0,0 +1,5 @@
+class Api::CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :courses
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #===== API =====
+  namespace :api, defaults: { format: "json "} do
+    resources :courses, only: [:index]
+    #resources :courses, controller: :courses, only: [:index]
+  end
 end
